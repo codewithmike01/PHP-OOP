d7b118a8fa96b8ff8952833b9893318a23476f3b
ginohmk <michotall95@gmail.com>
2022-08-04 10:58:58 +0100

    Add interaction add a new song moonlight

--- object_interaction.php
+++ object_interaction.php
@@ -22,13 +22,20 @@ $octopusSong = new Song();
 $octopusSong->songId = 1;
 $octopusSong->title= "Octopus's Garden";
 
+$moonLight = new Song();
+
+$moonLight->songId = 2;
+$moonLight->title = 'Moon light shine'; 
+
 // var_dump($octopusSong);
 
 // Playlist
 
 $playList = new PlayList();
+$playList->name = 'Rocks';
 
 $playList->addSong($octopusSong);
+$playList->addSong($moonLight);
 
 var_dump($playList->songs)
 ?>
