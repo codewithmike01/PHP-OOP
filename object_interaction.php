e54c3d8269451b22889346d8b2402e027b04471b
ginohmk <michotall95@gmail.com>
2022-08-04 10:53:31 +0100

    Add interaction of song to playlist

--- object_interaction.php
+++ object_interaction.php
@@ -6,13 +6,30 @@ class Song
   public $title;
 }
 
+class PlayList
+{
+  public $name;
+  public $songs = [];
+
+  public function addSong($song){
+    $this->songs[] = $song;
+  }
+}
+
+
 $octopusSong = new Song();
 
 $octopusSong->songId = 1;
 $octopusSong->title= "Octopus's Garden";
 
-var_dump($octopusSong);
+// var_dump($octopusSong);
 
+// Playlist
 
+$playList = new PlayList();
+
+$playList->addSong($octopusSong);
+
+var_dump($playList->songs)
 ?>
 
